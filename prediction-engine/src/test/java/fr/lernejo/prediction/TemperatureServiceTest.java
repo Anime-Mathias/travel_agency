8c1c40f45714f37abf367481d4f799be696941eb
Mathias Anime <mathias04@orange.fr>
2022-01-12 22:33:15 +0100

    Reconfiguration de TemperatureServiceTest
    Reconfiguration de : TemperatureServiceTest

--- prediction-engine/src/test/java/fr/lernejo/prediction/TemperatureServiceTest.java
+++ prediction-engine/src/test/java/fr/lernejo/prediction/TemperatureServiceTest.java
@@ -8,27 +8,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 class TemperatureServiceTest {
 
     private final TemperatureService service = new TemperatureService();
-<<<<<<< HEAD
 
-    @Test
-    void TempPaysRincon() {
-        Assertions.assertThatExceptionOfType(UnknownCountryException.class)
-            .isThrownBy(() -> service.getTemperature("hasard"))
-            .withMessage("Pays : 'hasard' inconnu.");
-    }
-
-    @ParameterizedTest
-    @CsvSource({
-        "France",
-        "france",
-        "FRANCE"
-    })
-    void TempFrance(String pays) {
-        double temperature = service.getTemperature(pays);
-        assertThat(temperature).isBetween(8D, 32D);
-    }
-=======
->>>>>>> e944e64 (Correction des bugs/Fix bug)
 }
 
 
